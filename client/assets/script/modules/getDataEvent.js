57dbd72d39beaf11fdc2c4ba42421f51d870fc16
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-08-26 15:06:38 +0200

    js- create function displayDataInTable

--- client/assets/script/modules/getDataEvent.js
+++ client/assets/script/modules/getDataEvent.js
@@ -1,25 +1,28 @@
+import {displayDataInTable} from "./displayDataInTable.js"
 /**
  * retrieves id, name, author and description for the event tables
- * @param {*} response 
+ * @param {*} response
  */
 export function getDataEvent(response) {
-    for (let event of response) {
-  console.log(response);
+  for (let event of response) {
+    //   console.log(response);
 
-  console.log(event.id);
-  console.log(event.name);
-  console.log(event.author);
-  console.log(event.description);
-  getDatesEvent(response)
+    //   console.log(event.id);
+    //   console.log(event.name);
+    //   console.log(event.author);
+    //   console.log(event.description);
+
+    displayDataInTable(event);
+    getDatesEvent(response);
   }
 }
 
 /**
  * retrieves dates and participants for the event tables
- * @param {*} response 
+ * @param {*} response
  */
- export function getDatesEvent(response) {
+export function getDatesEvent(response) {
   for (let dates of response[0].dates) {
-    console.log(dates);
+    // console.log(dates);
   }
 }
