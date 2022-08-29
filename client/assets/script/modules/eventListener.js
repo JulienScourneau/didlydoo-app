ed2c63ae6222e3b62150ef5cf441e5e4bf9bcf4d
Julien Scourneau <scourneaujulien@outlook.com>
2022-08-29 10:36:59 +0200

    Added conditions for openEvent function

--- client/assets/script/modules/eventListener.js
+++ client/assets/script/modules/eventListener.js
@@ -1,15 +1,16 @@
 import * as removeEvent from "../db/deleteEvent.js";
 
-export const openEvent = () => {
-    console.log("event click");
-    //window.getComputedStyle(div, null).getPropertyValue("display") == "none"? div.style.display = "block": div.style.display = "none";
+export const openEvent = (div) => {
+    window.getComputedStyle(div, null).getPropertyValue("display") == "none"
+        ? (div.style.display = "block")
+        : (div.style.display = "none");
 };
 
 export const editEvent = (id) => {
-    
+    console.log("edit");
 };
 
 export const deleteEvent = (id) => {
     console.log(id);
-    removeEvent.deleteEvent(id);
+    //removeEvent.deleteEvent(id);
 };
