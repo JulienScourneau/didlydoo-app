560c01b40c1bc4b992974822fe2fe6eb16e4986c
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-08-29 12:09:44 +0200

    js- edit --work in porgress

--- client/assets/script/modules/editEvent.js
+++ client/assets/script/modules/editEvent.js
@@ -4,5 +4,24 @@ export const editEvent = (id) => {
     let event = getEvent(id)
     event.then((json)=>{
         console.log(json);
+        displayEditEvent(json)
+        modifButton()
     })
 };
+
+function displayEditEvent(json) {
+    let div = document.getElementById("new-event")
+    div.style.display = "block"
+
+    document.getElementById('new-name').value = json.name;
+    document.getElementById('new-description').value = json.description;
+    document.getElementById('new-author').value = json.author;
+    // for (let element of json.dates) {
+    //     // document.getElementsByClassName('new-event__date__container__date__time')[0].textContent = element.date;
+    //     console.log(document.getElementById('new-event__date__container')[0].textContent);
+    // }
+};
+
+function modifButton() {
+    document.getElementById('add-new-event').innerHTML = "modifier"
+}
